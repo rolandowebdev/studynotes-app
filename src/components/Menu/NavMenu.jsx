dcfc1ddb15fddb880512d7f0d1a015d59e3e95e3
RolandoPranataAR <rolandopranata@gmail.com>
2022-07-28 05:07:36 +0700

    add: change font style

--- src/components/Menu/NavMenu.jsx
+++ src/components/Menu/NavMenu.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import InstagramIcon from "../../assets/instagram.svg";
-import SpotifyIcon from "../../assets/spotify.svg";
 import GithubIcon from "../../assets/github.svg";
 
 export default function NavMenu() {
@@ -9,17 +8,12 @@ export default function NavMenu() {
       <ul>
         <li>
           <a href='https://www.instagram.com/rolandowebdev/' target='_blank' rel='noreferrer'>
-            <img src={InstagramIcon} alt='Logo' />
+            <img src={InstagramIcon} alt='Logo' width={28} height={28} />
           </a>
         </li>
         <li>
           <a href='https://github.com/rolandopranata' target='_blank' rel='noreferrer'>
-            <img src={GithubIcon} alt='Logo' />
-          </a>
-        </li>
-        <li>
-          <a href='https://www.spotify.com/id/account/overview/' target='_blank' rel='noreferrer'>
-            <img src={SpotifyIcon} alt='Logo' />
+            <img src={GithubIcon} alt='Logo' width={28} height={28} />
           </a>
         </li>
       </ul>
